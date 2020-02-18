7151c4a832a6f8d96d6711593a5b5a8b670f1e49
denialp1 <48032321+denialp1@users.noreply.github.com>
2020-02-17 19:30:04 -0600

    Update output.java

--- NSPIP/src/output.java
+++ NSPIP/src/output.java
@@ -10,25 +10,24 @@ import java.util.*;
 
 public class output
 {
-	
-//	public static void printstockcalc(watchlistxpath find)
-//	{
-//		System.out.println("\nCompany: " + find.getXpath(0));
-//		System.out.print("Value: " + find.getXpath(1));
-//		System.out.print("  USD: " + find.getXpath(2));
-//		System.out.println(" " + find.getXpath(3) + "\n");
-//			
-//		System.out.printf("%-12s %-12s %-12s %-6s\n","Open: ", find.getXpath(4), "Div yield: ", find.getXpath(9));
-//		System.out.printf("%-12s %-12s %-12s %-6s\n", "High: ", find.getXpath(5), "Prev close: ", find.getXpath(10));
-//		System.out.printf("%-12s %-12s %-12s %-6s\n", "Low: ", find.getXpath(6), "52-wk high: ", find.getXpath(11));
-//		System.out.printf("%-12s %-12s %-12s %-6s\n", "Mkt cap: ", find.getXpath(7), "52=wk low: ", find.getXpath(12));
-//		System.out.printf("%-12s %-12s\n\n", "P/E ratio: ", find.getXpath(8));
-//		
-//	}	
-	
 	//name of company as parameter
-	public static void printstockcalc() {
+	public static void printstockcalc(String name) {
 		
+		stockcalc find = new stockcalc();
+		find.search(name);
+		
+		System.out.println("==================================================================");
+		System.out.println("\nCompany: " + find.getXpath(0));
+		System.out.print("Value: " + find.getXpath(1));
+		System.out.print("  USD: " + find.getXpath(2));
+		System.out.println(" " + find.getXpath(3) + "\n");
+			
+		System.out.printf("%-12s %-12s %-12s %-6s\n","Open: ", find.getXpath(4), "Div yield: ", find.getXpath(9));
+		System.out.printf("%-12s %-12s %-12s %-6s\n", "High: ", find.getXpath(5), "Prev close: ", find.getXpath(10));
+		System.out.printf("%-12s %-12s %-12s %-6s\n", "Low: ", find.getXpath(6), "52-wk high: ", find.getXpath(11));
+		System.out.printf("%-12s %-12s %-12s %-6s\n", "Mkt cap: ", find.getXpath(7), "52=wk low: ", find.getXpath(12));
+		System.out.printf("%-12s %-12s\n\n", "P/E ratio: ", find.getXpath(8));
+		System.out.println("==================================================================");
 	}
 	
 	// no parameter
@@ -43,6 +42,7 @@ public class output
 	
 	//no parameter
 	public static void printrecommendation() {
+		
 		System.out.print("\nLoading...\n\n");
 		//most actives
 		ArrayList<ArrayList> mostactives = new ArrayList<ArrayList>();
