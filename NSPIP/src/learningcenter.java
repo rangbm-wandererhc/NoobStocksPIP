b2df1297819f79f7faf26cc15a50fbabb7d6c3e1
denialp1 <48032321+denialp1@users.noreply.github.com>
2020-02-18 23:08:29 -0600

    Update learningcenter.java

--- NSPIP/src/learningcenter.java
+++ NSPIP/src/learningcenter.java
@@ -2,10 +2,9 @@
  * Program to retrieve stock data from google
  * Project Name: SPIP
  * 
- * @author Your Mom
+ * @author Daniel Park, Harang Kim
  * @version 2/16/2020
  */
-
 
 import java.util.ArrayList;
 import java.util.Scanner;
